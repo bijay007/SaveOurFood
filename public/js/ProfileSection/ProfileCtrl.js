6fff43ed1c95ba8ac73e45fee74d1ce26f340dd8
bijay timilsina <bjtimi.007@gmail.com>
2017-03-20 11:49:46 +0100

    few edits

--- public/js/ProfileSection/ProfileCtrl.js
+++ public/js/ProfileSection/ProfileCtrl.js
@@ -4,57 +4,53 @@
     .controller('ProfileCtrl', ProfileController)
 
   function ProfileController ($routeParams, $location, SaveFoodFactory, AuthFactory, $route, $scope, $rootScope) {
-    let vm = this
-
     $scope.$on('foodChanged', function () {
       SaveFoodFactory.getAllItems()
         .then((allItems) => {
-          vm.allFoodItems = allItems
+          $scope.allFoodItems = allItems
+          console.log(`value of allfood ${$scope.allFoodItems} ${allItems}`)
+          checkExpired($scope.allFoodItems)
+          SaveFoodFactory.getAllItems()
         })
-      console.log(`vale of vm.allfood ${vm.allFoodItems}`)
-        .then(checkExpired(vm.allFoodItems))
-        .then(SaveFoodFactory.getAllItems())
     })
 
       // typeof (elem.dateExpiring) === Date) ? elem.dateExpiring.split('').splice(0, 10).join(' ') : new Date()
-      // console.log(Date.parse(vm.allFoodItems[0].dateExpiring))
+      // console.log(Date.parse(allFoodItems[0].dateExpiring))
 
-    vm.addFood = function (e) {
+    $scope.addFood = function (e) {
       e.preventDefault()
-      var { foodName, quantity, dateBought, dateExpiring } = vm
+      var { foodName, quantity, dateBought, dateExpiring } = $scope
       dateExpiring = Date.parse('dateExpiring') || Date.parse(new Date()) + 8640000 // html5 date to timestamp
       dateBought = new Date('dateBought').getTime() || Date.now()
-      console.log(`datexpiring is ${dateExpiring} & datebought is ${dateBought} & ${dateExpiring - dateBought}`)
+      console.log(`datexpiring is ${dateExpiring} & datebought is ${dateBought}`)
       SaveFoodFactory.addItem({ foodName, quantity, dateBought, dateExpiring })
     }
 
-    vm.removeFood = function (e, elemId) {
+    $scope.removeFood = function (e, elemId) {
       e.preventDefault()
       console.log(`id captured from a element is ${elemId}`)
       SaveFoodFactory.removeItem(elemId)
     }
 
-    vm.editFood = function (data, id) {
+    $scope.editFood = function (data, id) {
       console.log(`${data} + ${id}`)
       // SaveFoodFactory.editItem({data.foodName, data.dateBought, data.dateExpiring, data.quantity, data.id})
     }
 
+    $scope.counterExpiring = 0
+    $scope.counterExpired = 0
+
     const checkExpired = function (allItems) {
-      vm.counterExpiring = 0
-      vm.counterExpired = 0
       for (var elem in allItems) {
         if (Date.parse(elem.dateExpiring) < Date.now()) {
-          vm.counterExpired += 1
+          $scope.counterExpired += 1
         } else if (Date.parse(elem.dateExpiring) > Date.now() && Date.parse(elem.dateExpiring) < Date.now() + 8640000) {
-          vm.counterExpiring += 1
-        } else {
-          console.log('fresh')
+          $scope.counterExpiring += 1
         }
-        console.log(vm.counterExpired + ' - ' + vm.counterExpiring)
       }
     }
 
-    vm.logout = function () {
+    $scope.logout = function () {
       AuthFactory.logout()
       $location.path('/login')
     }
