d13deae0bad20ad7cd764b75825f4690b7f486f8
bijay timilsina <bjtimi.007@gmail.com>
2017-03-24 11:17:01 +0100

    css input box on edit

--- public/js/components/foodStatus/foodStatusController.js
+++ public/js/components/foodStatus/foodStatusController.js
@@ -11,8 +11,8 @@
     var { counterExpiring, counterExpired } = vm.values
 
     if (counterExpiring <= 2 && counterExpired <= 2) vm.current = ' ❤ Great Job ! Your food are Fresh ❤'
-    else if ((counterExpiring > 2 || counterExpired > 2) && (counterExpiring < 5 || counterExpired < 5)) vm.current = '❗ Some Food are Expiring. Take a look ❗'
-    else if (counterExpired > 5 || counterExpiring > 5) vm.current = '💔 Your food are crying. Check them 💔'
-    else vm.current = 'Hmm Something seems fishy !!'
+    else if ((counterExpiring > 2 || counterExpired > 2) && (counterExpiring < 4 || counterExpired < 5)) vm.current = '❗ Some Food are Expiring. Take a look ❗'
+    else if (counterExpired > 4 || counterExpiring > 4) vm.current = '💔 Your food are crying. Check them 💔'
+    else vm.current = 'You have little food. Need to eat more !!'
   }
 })()
